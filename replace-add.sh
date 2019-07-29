1af3e860aa9537a7c38e1b0ed1b2ca11424d0232
@山有木兮木有枝 <31148279+cokezxs@users.noreply.github.com>
2019-07-29 16:39:03 +0800

    Update replace-add.sh

--- replace-add.sh
+++ replace-add.sh
@@ -1,9 +1,11 @@
 #bin/bash
 cd /tmp
+# haha文件存放的是不规则网址
 a=($(cat haha))
 for ((i=0;i<${#a[@]};i++))
  do
   echo ${a[i]}
+  # 批量替换网址
   sed -i "s@${a[i]}@${a[i]}  https://${a[i]}/login.html@g" haha
  done
 
